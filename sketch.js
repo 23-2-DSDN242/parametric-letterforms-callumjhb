a0d0338f842cd372cfc6f3d0abaae2f8a8ff232d
callumjhb <callumjhbenfell@gmail.com>
2024-04-25 13:29:27 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,31 +1,19 @@
 const canvasWidth = 960;
 const canvasHeight = 500;
 
-/*
- * my three variable per letter are:
- *
-   size: radius of the second circle (in pixels)
-   offsetx: x offset (in pixels) of the second circle
-            relative to the first one
-   offsety: y offset (in pixels) of the second circle
-            relative to the first one
- *
- */
-
 const letterA = {
-  "trianglesize": 100,
   "triangleorientation": 1,
 
   "circle1size": 0,
   "circle2size": 0,
   "circlex": 0,
-  "circle1y": 0,
-  "circle2y": 0,
+  "circle1y": -80,
+  "circle2y": -80,
 
-  "linewidth": 20,
-  "lineheight": 10,
-  "linex": 10,
-  "liney": -30,
+  "linewidth": 15,
+  "lineheight": 58,
+  "linex": 0,
+  "liney": 50,
 
   "rectanglewidth": 0,
   "rectangleheight": 0,
@@ -34,53 +22,53 @@ const letterA = {
 }
 
 const letterB = {
+  "trianglesize": 0,
+  "triangleorientation": 0,
+  
   "circle1size": 100,
   "circle2size": 100,
   "circlex": 12.5,
   "circle1y": -50,
   "circle2y": 50,
 
-  "lineheight": 30,
   "linewidth": 15,
+  "lineheight": 30,
   "linex": 12.5,
   "liney": 35,
 
-  "rectangleheight": 200,
   "rectanglewidth": 25,
+  "rectangleheight": 200,
   "rectanglex": -62.5,
-  "rectangley": -100,
-
-  "trianglesize": 0,
-  "triangleorientation": 1
+  "rectangley": -100
 }
 
 const letterC = {
+  "trianglesize": 0,
+  "triangleorientation": 0,
+
   "circle1size": 50,
   "circle2size": 50,
   "circlex": 25,
   "circle1y": -75,
   "circle2y": 75,
 
-  "lineheight": 0,
   "linewidth": 0,
+  "lineheight": 0,
   "linex": 0,
   "liney": 0,
 
-  "rectangleheight": 200,
   "rectanglewidth": 50,
+  "rectangleheight": 200,
   "rectanglex": -50,
-  "rectangley": -100,
-
-  "trianglesize": 0,
-  "triangleorientation": 1
+  "rectangley": -100
 }
 
 const backgroundColor  = "#fffae7";
 
-const darkGreen  = "#26b29d";
-const lightGreen  = "#30dfc4";
+const yellow  = "#FFE75E";
+const red  = "#FB5959";
+const blue = "#82B0EF";
 const strokeColor  = "#000000";
-const yellow = "#fffc33";
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -93,7 +81,6 @@ function setup () {
 
   // with no animation, redrawing the screen is not necessary
   noLoop();
-
 }
 
 function draw () {
@@ -112,9 +99,12 @@ function draw () {
 
 function drawLetter(posx, posy, letterData) {
   // determine parameters for triangle   
-  let triPosx = posx + letterData["trianglesize"];
-  let triPosy = posy + letterData["trianglesize"];
-  let triAngle = letterData["triangleorientation"];
+  let triPointx = posx;
+  let triPointy = posy-85*letterData["triangleorientation"];
+  let triLeftx = posx-65*letterData["triangleorientation"];
+  let triLefty = posy+100*letterData["triangleorientation"];
+  let triRightx = posx+65*letterData["triangleorientation"];
+  let triRighty = posy+100*letterData["triangleorientation"];
   
   // determine parameters for first circle
   let circSize1 = letterData["circle1size"];
@@ -136,29 +126,27 @@ function drawLetter(posx, posy, letterData) {
   let lineHeight = letterData["lineheight"];
   let linePosx = posx + letterData["linex"];
   let linePosy = posy + letterData["liney"];
-
   
   //draw triangle
-  triangle(0, triSize * -triAngle, triSize * -triAngle,triSize * triAngle, triSize * -triAngle,triSize * triAngle);
+  fill(blue);
+  triangle(triPointx, triPointy, triLeftx, triLefty, triRightx, triRighty);
   
   // draw first circle
-  fill(lightGreen);
+  fill(red);
   ellipse(circPosx, circPos1y, circSize1, circSize1);
 
   // draw second circle
-  fill(lightGreen);
+  fill(red);
   ellipse(circPosx, circPos2y, circSize2, circSize2);
 
   // draw rectangle
+  fill(yellow);
   rect(rectPosx, rectPosy, rectWidth, rectHeight);
 
   // draw lines
   line(linePosx-lineWidth, linePosy, linePosx-lineWidth, linePosy+lineHeight);
   line(linePosx, linePosy, linePosx, linePosy+lineHeight);  
   line(linePosx+lineWidth, linePosy, linePosx+lineWidth, linePosy+lineHeight);
-  
- 
-
 }
 
 function keyTyped() {
